35130142f408772b397be4c9c121a1a8b238a5a1
nklalonde <31867179+nklalonde@users.noreply.github.com>
2018-10-25 21:17:09 -0500

    SignIn
    Sign in form is ready. I have made a main form that needs to be edited. We need to figure out how to link the database to the program.

---
+++ LMS/LMS/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace LMS
+{
+    partial class Login_Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login_Window));
+            this.Username_Textbox = new System.Windows.Forms.TextBox();
+            this.Password_Textbox = new System.Windows.Forms.TextBox();
+            this.Login_Button = new System.Windows.Forms.Button();
+            this.Exit_Button = new System.Windows.Forms.Button();
+            this.Username_Label = new System.Windows.Forms.Label();
+            this.Password_Label = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Username_Textbox
+            // 
+            this.Username_Textbox.BackColor = System.Drawing.SystemColors.Menu;
+            this.Username_Textbox.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username_Textbox.Location = new System.Drawing.Point(1063, 201);
+            this.Username_Textbox.Name = "Username_Textbox";
+            this.Username_Textbox.Size = new System.Drawing.Size(408, 74);
+            this.Username_Textbox.TabIndex = 0;
+            this.Username_Textbox.TextChanged += new System.EventHandler(this.Username_Textbox_TextChanged);
+            // 
+            // Password_Textbox
+            // 
+            this.Password_Textbox.BackColor = System.Drawing.SystemColors.Menu;
+            this.Password_Textbox.Font = new System.Drawing.Font("Franklin Gothic Medium Cond", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password_Textbox.Location = new System.Drawing.Point(1063, 337);
+            this.Password_Textbox.Name = "Password_Textbox";
+            this.Password_Textbox.Size = new System.Drawing.Size(408, 74);
+            this.Password_Textbox.TabIndex = 1;
+            this.Password_Textbox.TextChanged += new System.EventHandler(this.Password_Textbox_TextChanged);
+            // 
+            // Login_Button
+            // 
+            this.Login_Button.Font = new System.Drawing.Font("Franklin Gothic Heavy", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Login_Button.Location = new System.Drawing.Point(721, 499);
+            this.Login_Button.Name = "Login_Button";
+            this.Login_Button.Size = new System.Drawing.Size(293, 124);
+            this.Login_Button.TabIndex = 2;
+            this.Login_Button.Text = "Login";
+            this.Login_Button.UseVisualStyleBackColor = true;
+            this.Login_Button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Exit_Button
+            // 
+            this.Exit_Button.Font = new System.Drawing.Font("Franklin Gothic Heavy", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Exit_Button.Location = new System.Drawing.Point(1178, 499);
+            this.Exit_Button.Name = "Exit_Button";
+            this.Exit_Button.Size = new System.Drawing.Size(293, 124);
+            this.Exit_Button.TabIndex = 3;
+            this.Exit_Button.Text = "E&xit";
+            this.Exit_Button.UseVisualStyleBackColor = true;
+            this.Exit_Button.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Username_Label
+            // 
+            this.Username_Label.AutoSize = true;
+            this.Username_Label.Font = new System.Drawing.Font("Franklin Gothic Heavy", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username_Label.ForeColor = System.Drawing.Color.White;
+            this.Username_Label.Location = new System.Drawing.Point(708, 204);
+            this.Username_Label.Name = "Username_Label";
+            this.Username_Label.Size = new System.Drawing.Size(349, 74);
+            this.Username_Label.TabIndex = 4;
+            this.Username_Label.Text = "User Name:";
+            this.Username_Label.Click += new System.EventHandler(this.Username_Label_Click);
+            // 
+            // Password_Label
+            // 
+            this.Password_Label.AutoSize = true;
+            this.Password_Label.Font = new System.Drawing.Font("Franklin Gothic Heavy", 22.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password_Label.ForeColor = System.Drawing.Color.White;
+            this.Password_Label.Location = new System.Drawing.Point(708, 334);
+            this.Password_Label.Name = "Password_Label";
+            this.Password_Label.Size = new System.Drawing.Size(319, 74);
+            this.Password_Label.TabIndex = 5;
+            this.Password_Label.Text = "Password:";
+            this.Password_Label.Click += new System.EventHandler(this.Password_Label_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(667, 865);
+            this.panel1.TabIndex = 7;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(661, 868);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Franklin Gothic Heavy", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(79, 724);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(442, 161);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "LOGIN";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Login_Window
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1665, 919);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Password_Label);
+            this.Controls.Add(this.Username_Label);
+            this.Controls.Add(this.Exit_Button);
+            this.Controls.Add(this.Login_Button);
+            this.Controls.Add(this.Password_Textbox);
+            this.Controls.Add(this.Username_Textbox);
+            this.Name = "Login_Window";
+            this.Text = "Login Window";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Username_Textbox;
+        private System.Windows.Forms.TextBox Password_Textbox;
+        private System.Windows.Forms.Button Login_Button;
+        private System.Windows.Forms.Button Exit_Button;
+        private System.Windows.Forms.Label Username_Label;
+        private System.Windows.Forms.Label Password_Label;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ LMS/LMS/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LMS
+{
+    public partial class Login_Window : Form
+    {
+        public Login_Window()
+        {
+            InitializeComponent();
+        }
+        // Exit Button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        // Log in button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Main ss = new Main();
+            ss.Show();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Username_Label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Password_Label_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Username_Textbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Password_Textbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
